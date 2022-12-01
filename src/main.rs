8b45c250faa07e7fe1cb49a472c38732754ebf17
andreapefe <andreaperezfdez@gmail.com>
2022-12-01 15:53:40 +0100

    change tx et rx pin

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,12 @@ use core::num::{NonZeroU16, NonZeroU8};
 
 use cortex_m_rt::entry;
 
-use log::info;
+//use log::hprintln;
 use stm32g4xx_hal::fdcan::id::Id;
 
 use cortex_m_semihosting::hprintln;
+use stm32g4xx_hal::delay::SYSTDelayExt;
+use stm32g4xx_hal::time::Hertz;
 
 
 #[macro_use]
@@ -34,10 +36,10 @@ mod utils;
 fn main() -> ! {
     utils::logger::init();
 
-    info!("Start");
+    //hprintln!("Start");
 
     // APB1 (HSE): 24MHz, Bit rate: 125kBit/s, Sample Point 87.5%
-    // Value was calculated with http://www.bittiming.can-wiki.info/
+    // Value was calculated with http://www.bittiming.can-wiki.hprintln/
     // TODO: use the can_bit_timings crate
     let btr = NominalBitTiming {
         prescaler: NonZeroU16::new(12).unwrap(),
@@ -46,50 +48,50 @@ fn main() -> ! {
         sync_jump_width: NonZeroU8::new(1).unwrap(),
     };
 
-    info!("Init Clocks");
+    //hprintln!("Init Clocks");
 
     let dp = Peripherals::take().unwrap();
     let _cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
     let rcc = dp.RCC.constrain();
-    let mut rcc = rcc.freeze(Config::new(SysClockSrc::HSE(24.mhz())));
+    let mut rcc = rcc.freeze(Config::new(SysClockSrc::HSI));
 
-    info!("Split GPIO");
+    //hprintln!("Split GPIO");
 
     let gpioa = dp.GPIOA.split(&mut rcc);
 
     let can1 = {
-        info!("Init CAN 1");
+        //hprintln!("Init CAN 1");
         let rx = gpioa.pa11.into_alternate().set_speed(Speed::VeryHigh);
         let tx = gpioa.pa12.into_alternate().set_speed(Speed::VeryHigh);
 
-        info!("-- Create CAN 1 instance");
+        //hprintln!("-- Create CAN 1 instance");
         let can = FdCan::new(dp.FDCAN1, tx, rx, &rcc);
 
-        hprintln!("-- Set CAN 1 in Config Mode");
+        //hprintln!("-- Set CAN 1 in Config Mode");
         let mut can = can.into_config_mode();
         can.set_protocol_exception_handling(false);
 
-        info!("-- Configure nominal timing");
+        //hprintln!("-- Configure nominal timing");
         can.set_nominal_bit_timing(btr);
 
-        info!("-- Configure Filters");
+        //hprintln!("-- Configure Filters");
         can.set_standard_filter(
             StandardFilterSlot::_0,
             StandardFilter::accept_all_into_fifo0(),
         );
 
-        info!("-- Current Config: {:#?}", can.get_config());
+        //hprintln!("-- Current Config: {:#?}", can.get_config());
 
-        info!("-- Set CAN1 in to normal mode");
+        //hprintln!("-- Set CAN1 in to normal mode");
         // can.into_external_loopback()
         can.into_normal()
     };
 
     let mut can = can1;
 
-    info!("Create Message Data");
+    //hprintln!("Create Message Data");
     let mut buffer = [0xAAAAAAAA, 0xFFFFFFFF, 0x0, 0x0, 0x0, 0x0];
-    info!("Create Message Header");
+    //hprintln!("Create Message Header");
     let header = TxFrameHeader {
         len: 2 * 4,
         id: StandardId::new(0x1).unwrap().into(),
@@ -97,14 +99,16 @@ fn main() -> ! {
         bit_rate_switching: false,
         marker: None,
     };
-    info!("Initial Header: {:#X?}", &header);
+    //hprintln!("Initial Header: {:#X?}", &header);
 
-    info!("Transmit initial message");
+    //hprintln!("Transmit initial message");
     block!(can.transmit(header, &mut |b| {
         let len = b.len();
         b[..len].clone_from_slice(&buffer[..len]);
     },))
         .unwrap();
+
+    let mut delay = _cp.SYST.delay(&rcc.clocks);
 
     loop {
 
@@ -116,6 +120,7 @@ fn main() -> ! {
             marker : None
         };
 
+        hprintln!("In");
 
         block!(
             can.transmit(txheader, &mut |b| {
@@ -124,5 +129,7 @@ fn main() -> ! {
                 hprintln!("Transmit: {:X?}", b);
             })
         ).unwrap();
+
+        delay.delay_ms(100_u32);
     }
 }
