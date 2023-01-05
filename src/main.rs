580b03e90230f62a13e2f142a82cd7e3db919e3f
triet_ng <tr_nguye@insa-toulouse.fr>
2023-01-05 17:56:19 +0100

    Configure USART (tranmission pin) and test OK

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_main]
 #![no_std]
 
+
 use hal::delay::DelayFromCountDownTimer;
 use hal::prelude::*;
 use hal::rcc::Config;
@@ -8,15 +9,16 @@ use hal::stm32;
 use hal::timer::Timer;
 use stm32g4xx_hal as hal;
 use cortex_m_semihosting::hprintln;
-
 use cortex_m_rt::entry;
 use log::info;
+use hal::serial;
+use stm32g4xx_hal::serial::FullConfig;
 
 #[macro_use]
 mod utils;
 
 #[entry]
-fn main() -> ! {
+fn uart() -> ! {
     utils::logger::init();
 
     hprintln!("start");
@@ -24,9 +26,7 @@ fn main() -> ! {
     let cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
     let mut rcc = dp.RCC.freeze(Config::hsi());
 
-    hprintln!("Init Led");
     let gpioa = dp.GPIOA.split(&mut rcc);
-    let mut led = gpioa.pa5.into_push_pull_output();
 
     hprintln!("Init SYST delay");
     let mut delay_syst = cp.SYST.delay(&rcc.clocks);
@@ -35,16 +35,23 @@ fn main() -> ! {
     let timer2 = Timer::new(dp.TIM2, &rcc.clocks);
     let mut delay_tim2 = DelayFromCountDownTimer::new(timer2.start_count_down(100.ms()));
 
-    loop {
+    //Configure the baud rate for USART communication
 
-        //led.set_low();
-        hprintln!("Toggle");
-        led.toggle().unwrap();
-        hprintln!("SYST delay");
-        delay_syst.delay(1000.ms());
-        hprintln!("Toggle");
-        led.toggle().unwrap();
-        hprintln!("TIM2 delay");
-        delay_tim2.delay_ms(1000_u16);
+    let pin_tx = gpioa.pa2.into_alternate();
+    let pin_rx = gpioa.pa3.into_alternate();
+
+    let mut usart = serial::usart::SerialExt::usart(
+        dp.USART2,
+        (pin_tx),
+        (pin_rx),
+        serial::FullConfig::default(),
+        (&mut rcc),
+    ).unwrap();
+
+    loop {
+        hprintln!("Begin of loop");
+        delay_tim2.delay_ms(1000 as u16);
+        hprintln!("Sent data");
+        usart.write(6).unwrap();
     }
 }
